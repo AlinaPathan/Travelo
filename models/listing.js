b057f76e57554809e9f257fe56b61fd590e8a4f6
AlinaPathan <alinapathan2004@gmail.com>
2025-04-22 16:28:25 +0530

    validated'

--- models/listing.js
+++ models/listing.js
@@ -1,17 +1,18 @@
 const mongoose=require("mongoose")
 const Schema=mongoose.Schema;
-const defaultImg="https://unsplash.com/photos/white-and-blue-sea-waves-painting-63RnGWHVIXs"
+const defaultImg="https://unsplash.com/photos/a-lake-with-mountains-in-the-background-and-clouds-in-the-sky-GxSAX1Du5-o";
+
 const listingSchema=new Schema({
     title: {
         type:String,
         required:true
     },
     description:String,
-    image:{
-        type:String,
-        default:"https://unsplash.com/photos/white-and-blue-sea-waves-painting-63RnGWHVIXs",
-        set:(v)=>v==""?defaultImg:v,
-    },
+    image: {
+        type: String,
+        default: defaultImg,
+        set: (v) => !v ? defaultImg : v
+      },
     price:String,
     location:String,
     country:String
