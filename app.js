7c00a62a545efb1063e935289903a7379bca8ac2
AlinaPathan <alinapathan2004@gmail.com>
2025-04-22 22:31:13 +0530

    error handling done

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(methodOverride("_method"))
 app.engine("ejs",ejsMate)
 app.use(express.static(path.join(__dirname,"/public")))
 const wrapAsync=require("./utils/wrapAsync.js")
+const ExpressError=require("./utils/ExpressError.js")
 //connecting database
 const MONGO_URL='mongodb://127.0.0.1:27017/travelo';
 async function main(){
@@ -24,10 +25,10 @@ main().then(()=>{
 
 })
 //alllist home page
-app.get("/listing",async(req,res)=>{
+app.get("/listing",wrapAsync(async(req,res)=>{
  const allListing= await Listing.find({})
  res.render("listings/listing.ejs",{allListing})
-})
+}))
 //newww
 app.get("/listing/new",(req,res)=>{
     res.render("listings/new.ejs")
@@ -39,44 +40,47 @@ let newListing=new Listing(req.body.listing);
 await newListing.save()
  res.redirect("/listing")
   
-    next(err);
+  
 }));
 
 //show route 
-app.get("/listing/:id",async (req,res)=>{
+app.get("/listing/:id",wrapAsync(async (req,res)=>{
  let {id}=req.params;
 const listing= await Listing.findById(id);
 res.render("listings/show.ejs",{listing})
-})
+}))
 
 //Edit listing
-app.get("/listing/:id/edit",async(req,res)=>{
+app.get("/listing/:id/edit",wrapAsync(async(req,res)=>{
     let {id}=req.params;
 const listing= await Listing.findById(id);
 res.render("listings/edit.ejs",{listing})
-})
+}))
 //post edited listing
-app.put("/listing/:id",async(req,res)=>{
+app.put("/listing/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
    await Listing.findByIdAndUpdate(id,{...req.body.listing})
     // {...req.body.listing} is for the reconstruction of the listing object with new values.
    res.redirect(`/listing/${id}`)//redirect to show page
-})
+}))
 
 //delete listing
-app.delete("/listing/:id",async(req,res)=>{
+app.delete("/listing/:id",wrapAsync(async(req,res)=>{
   let {id}=req.params;
   let deletedListing= await Listing.findByIdAndDelete(id)
   console.log(deletedListing)
   res.redirect("/listing")
 })
-
+)
 app.get("/",(req,res)=>{
     res.send("working")
 })
-
+app.all("*",(req,res,next)=>{
+    next(new ExpressError(400,"Page not found!!"));
+})
 app.use((err,req,res,next)=>{
-    res.send("something went wrong");
+   let{statusCode,message}=err;
+   res.status(statusCode).send(message);
 })
 
 app.listen(3000,()=>{
